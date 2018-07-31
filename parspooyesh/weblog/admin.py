9d47c5d4e553a81273ced740de5ef8db81a63a23
faridlotfali <farid9lotfali@gmail.com>
2018-07-31 11:23:18 +0430

    develop :#11 add comment to post in admin page

--- parspooyesh/weblog/admin.py
+++ parspooyesh/weblog/admin.py
@@ -6,7 +6,25 @@ from django.contrib.auth.models import User
 from .models import Comment,slider
 from .models import Post,Profile
 
-admin.site.register(Post)
+
+admin.site.site_header = "ParsPooyesh Admin"
+admin.site.site_title = "ParsPooyesh Admin Portal"
+admin.site.index_title = "Welcome to ParsPooyesh Portal"
+
+# class QuestionAdmin(admin.ModelAdmin):
+    # fields = ['pub_date', 'post_text']
+class ChoiceInline(admin.StackedInline):
+    model = Comment
+    extra = 2
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+    ('lock',{'fields': ['post_text']}),
+    ('Date information', {'fields': ['pub_date']}),
+    ]
+    inlines = [ChoiceInline]
+
+admin.site.register(Post,QuestionAdmin)
 admin.site.register(Comment)
 admin.site.register(slider)
 admin.site.register(Profile)
@@ -18,7 +36,7 @@ class ProfileInline(admin.StackedInline):
     fk_name = 'user'
 
 class CustomUserAdmin(UserAdmin):
-    inlines = (ProfileInline, )
+    inlines = (ProfileInline,)
 
     def get_inline_instances(self, request, obj=None):
         if not obj:
